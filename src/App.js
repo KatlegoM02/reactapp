2fa2e9385faf3291683867d0534a0782276c89d2
KatlegoM <katlegomakgatle@gmail.com>
2024-07-17 16:53:15 +0200

    renamed the file and removed unnecessary  files

--- src/App.js
+++ src/App.js
@@ -5,16 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> The Weather App</h1>
-        <Weather />
-      </header>
-      <p class="text-center mt-5">
-        ✌️ This website was coded by Katlego Makgatle, and it is 
-        <a href="https://github.com/KatlegoM02/reactapp" target="_blank">
-          open-sourced
-        </a>
-      </p>
+      Weather App
     </div>
   );
 }
